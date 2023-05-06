3f96dfa8510744935cbf9ac89d593945ba851951
yach36 <yach0306@gmail.com>
2023-05-07 01:08:27 +0900

    feat: Counterコンポーネントの追加

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,11 @@
 import UsersList from "./UsersList"
+import Counter from "./Counter"
+
 const Test = () => {
   return (
     <>
       <h1 className="text-xl font-bold">Testページ</h1>
+      <Counter />
       {/* @ts-expect-error Server Component */}
       <UsersList />
     </>
