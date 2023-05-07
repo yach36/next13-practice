1c71165adfe89ecdd1285bf72a5e4087d4490c03
yach36 <yach0306@gmail.com>
2023-05-07 12:41:02 +0900

    feature: 遅延処理の追加

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,16 @@
 import UsersList from "./UsersList"
 import Counter from "./Counter"
+import { Suspense } from "react"
 
 const Test = () => {
   return (
     <>
       <h1 className="text-xl font-bold">Testページ</h1>
       <Counter />
-      {/* @ts-expect-error Server Component */}
-      <UsersList />
+      <Suspense fallback={<p className="mt-4">ユーザデータ Loading...</p>}>
+        {/* @ts-expect-error Server Component */}
+        <UsersList />
+      </Suspense>
     </>
   )
 }
